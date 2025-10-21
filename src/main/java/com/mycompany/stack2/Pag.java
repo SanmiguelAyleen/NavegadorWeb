a1e656cee281929a63bc94462e53c7854728ba4b
SanmiguelAyleen <ayleen.dessiresanmiguel@gmail.com>
2025-10-21 16:57:44 -0500

    Taller grupa: Navegador

---
+++ src/main/java/com/mycompany/stack2/Pag.java
@@ -0,0 +1,45 @@
+package com.mycompany.stack2;
+
+import java.util.Stack;
+
+public class Pag {
+
+    private String actual;
+    private Stack<String> atras = new Stack<>();
+    private Stack<String> adelante = new Stack<>();
+
+    public Pag(String actual) {
+
+        this.actual = actual;
+        atras = new Stack<>();
+        adelante = new Stack<>();
+    }
+
+    public boolean navegarAdelante() {
+
+        if (adelante.isEmpty()) {
+            return false;
+        } else {
+            atras.push(actual);
+            actual = adelante.peek();
+            adelante.pop();
+
+            return true;
+        }
+    }
+
+    public boolean navegarAtras() {
+
+        if (atras.isEmpty()) {
+            return false;
+
+        } else {
+
+            adelante.push(actual);
+            actual = atras.peek();
+            atras.pop();
+            return true;
+        }
+
+    }
+}
---
+++ src/main/java/com/mycompany/stack2/PaginaWeb.java
@@ -0,0 +1,19 @@
+package com.mycompany.stack2;
+
+/**
+ *
+ * @author USER
+ */
+public class PaginaWeb {
+
+    public static void main(String[] args) {
+
+        Pag web = new Pag("https//Espoch.edu.ec");
+        /*Pag web2 = new Pag("https//Espoch.edu.ec");
+        Pag web3 = new Pag("https//Espoch.edu.ec");
+
+        web.navegarAdelante();
+
+        web.navegarAtras();*/
+    }
+}
